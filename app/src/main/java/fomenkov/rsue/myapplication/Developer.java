0ef6886c99d20f652ccb59de1f4265271f5a4b63
chirfer <chiefyes05@gmail.com>
2024-12-10 02:45:37 +0300

    developer

---
+++ app/src/main/java/fomenkov/rsue/myapplication/Developer.java
@@ -0,0 +1,33 @@
+package fomenkov.rsue.myapplication;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Developer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_developer);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void Click(View view) {
+        Intent intent = new Intent(Developer.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
